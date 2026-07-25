6d9b1d47bb40099f2fa3be3fcf9bfb20ecbdcd88
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-07-25 17:03:42 -0500

    chore(db): remove duplicate ledger uniqueness migration

--- migrations/0027_ledger_account_company_code_unique.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE ledger_accounts
-  DROP CONSTRAINT IF EXISTS ledger_accounts_code_unique;
-
-DROP INDEX IF EXISTS ledger_accounts_code_unique;
-
-CREATE UNIQUE INDEX IF NOT EXISTS ledger_accounts_company_code_unique
-  ON ledger_accounts (company_id, code);
